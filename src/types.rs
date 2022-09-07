a3491938200fb2a57e6b02290ae2063af787bd68
notseanray <seanray410@gmail.com>
2022-09-07 14:33:45 -0400

    more struct definitions

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,15 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-pub struct Config {
+pub(crate) struct Config {
     #[serde(default = "default_host")]
     pub host: String,
     pub port: u16,
     pub client_origin_url: String,
+    max_songs: usize,
+    max_song_folder_size_gb: usize,
+    retries: usize,
+    yt_timeout_ms: usize,
 }
 
 fn default_host() -> String {
@@ -19,10 +23,34 @@ impl Default for Config {
 }
 
 #[derive(Serialize)]
-pub struct ErrorMessage {
+pub(crate) struct ErrorMessage {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub error_description: Option<String>,
     pub message: String,
+}
+
+pub(crate) struct User {
+    username: String,
+    password: String,
+    public: bool,
+    last_played: VecDeque<String>,
+    display_name: String,
+    followers: Vec<String>,
+    following: Vec<String>,
+    analytics: bool,
+    admin: bool,
+}
+
+pub(crate) struct Song {
+    id: String,
+    title: String,
+    album: Option<String>,
+    artist: String,
+    duration: f64,
+    genre: Option<String>,
+    track_disc: [u16; 2],
+    album_arist: Vec<String>,
+    size: u64,
 }
